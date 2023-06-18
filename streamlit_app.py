82f88da52b48f351d03aa1ea999272c62e32734a
Payam Ardeshirzad <payam.ardeshirzad@gmail.com>
2023-06-18 15:00:56 +1200

    Update streamlit_app.py
    Added breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 streamlit.title('My Parents new Healthy Diner')
+streamlit.header('Breakfast Menu')
 streamlit.text('Omega 2 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
