efadb33d3acac22f275a9abe91c6e963e9c7a368
Payam Ardeshirzad <payam.ardeshirzad@gmail.com>
2023-06-29 12:22:36 +1200

    Update streamlit_app.py
    - add test code to display dummy data

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,3 +31,5 @@ streamlit.text(fruityvice_response.json())
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Display dataframe as an interactive table
 streamlit.dataframe(fruityvice_normalized)
+streamlit.text(fruityvice_response.json())
+
