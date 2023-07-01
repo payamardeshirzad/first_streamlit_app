9faa83434991d0e21c7ae0057342ed568664a110
Payam Ardeshirzad <payam.ardeshirzad@gmail.com>
2023-07-01 21:13:07 +1200

    Update streamlit_app.py
    imported snowflake.connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,4 +34,4 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 # Display dataframe as an interactive table
 streamlit.dataframe(fruityvice_normalized)
-
+import snowflake.connector
